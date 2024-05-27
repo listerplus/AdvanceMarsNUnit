55b8f70ef11c8617f07df51fea8fc2bc73d7caf0
listerplus <96763963+listerplus@users.noreply.github.com>
2024-05-27 10:56:02 +1000

    Add Utilities

---
+++ Utils/Util.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdvanceMarsNUnit.Utils
+{
+    public class Util
+    {
+        public static string GetProjRootDir()
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            return currentDir.Split("bin")[0];
+        }
+    }
+}
